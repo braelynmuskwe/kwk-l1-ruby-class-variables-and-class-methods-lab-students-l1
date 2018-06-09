75863f9fb35362eb94a30d194bebba89bf33f388
braelynmuskwe <braemira1@icloud.com>
2018-06-09 01:09:17 +0000

    automatically backed up by learn

---
+++ login.rb
@@ -0,0 +1,42 @@
+# class User
+  
+#   def name=(username)
+#     @the_username = username
+#   end
+  
+#   def name 
+#     @the_username
+#   end 
+# end
+
+# jessica = User.new
+# jessica.name = "Jessica"
+# puts jessica.name
+
+class User 
+  attr_accessor :username, :password, :email, :age
+  #this is always initialize MAKE SURE YOU SPELL IT RIGHT
+  def initialize(username, password, email, age)
+    @username=username
+    @password=password
+    @email=email 
+    @age=age
+  end
+   end  
+    
+    
+    
+    jessica = User.new("jessica", "password", "jessica@jessica.com", "19")
+    sofia = User.new("sofia is cool", "123 cats", "sofia@sof.com", "17")
+    puts jessica.username
+    puts jessica.password
+    puts jessica.email
+    puts jessica.age
+    
+    puts sofia.username
+    puts sofia.password
+    puts sofia.email
+    puts sofia.age
+    
+    
+    
